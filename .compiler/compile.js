e8d4b7f924a7d7c6d699c37aa920b71fc3d971b0
TheCondemner <1tymur.tykva1@gmail.com>
2022-04-19 19:14:47 +0200

    Initial commit v2

---
+++ .compiler/compile.js
@@ -0,0 +1,97 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.compile = void 0;
+/* -------------------------------------------------------------------------- */
+/*                                   Imports                                  */
+/* -------------------------------------------------------------------------- */
+const path = __importStar(require("path"));
+const fs = __importStar(require("fs"));
+/* -------------------------------------------------------------------------- */
+/*                                   Runtime                                  */
+/* -------------------------------------------------------------------------- */
+function compile(name) {
+    const filePath = path.join(__dirname, `../source/${name}.bf`);
+    const compiledFilePath = path.join(__dirname, `../out/${name}.js`);
+    const validCode = ['+', '-', '.', ',', '[', ']', '<', '>'];
+    const fileContent = fs
+        .readFileSync(filePath, 'utf8')
+        .split('')
+        .filter(function (character) {
+        return validCode.includes(character);
+    });
+    let compiledFileContent = '/* ------------------------------- Code Header ------------------------------ */ \n' +
+        'const input = require("readline-sync"); \n\n' +
+        'let data = new Array(30000).fill(0); \n' +
+        'let pointer = 0; \n\n' +
+        '/* ------------------------------ Compiled Code ----------------------------- */ \n';
+    const append = (code) => {
+        compiledFileContent += code + ' \n';
+    };
+    fileContent.forEach((character) => {
+        //? The switch case, oh god, the switch case
+        switch (character) {
+            case '+': {
+                append('data[pointer] += 1;');
+                break;
+            }
+            case '-': {
+                append('data[pointer] -= 1;');
+                break;
+            }
+            case '<': {
+                append('if (pointer -= 1 > 0) { pointer -= 1; } else { pointer = 29999; };');
+                break;
+            }
+            case '>': {
+                append('if (pointer += 1 < 30000) { pointer += 1; } else { pointer = 0; };');
+                break;
+            }
+            case ',': {
+                append('data[pointer] = input.question("").charCodeAt(0);');
+                break;
+            }
+            case '.': {
+                append('console.log(String.fromCharCode(data[pointer]));');
+                break;
+            }
+            case '[': {
+                append('while (data[pointer] > 0) {');
+                break;
+            }
+            case ']': {
+                append('};');
+                break;
+            }
+            default: {
+                break;
+            }
+        }
+        ;
+    });
+    //? Final Write
+    fs.writeFileSync(compiledFilePath, compiledFileContent);
+}
+exports.compile = compile;
---
+++ .compiler/main.js
@@ -0,0 +1,49 @@
+"use strict";
+/* ---------------------------------- Notes --------------------------------- */
+//TODO Add catch exceptions n stuff
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+/* -------------------------------------------------------------------------- */
+/*                                   Imports                                  */
+/* -------------------------------------------------------------------------- */
+const readline_sync_1 = require("readline-sync");
+const validFiles = __importStar(require("./locate"));
+const compile_1 = require("./compile");
+/* -------------------------------------------------------------------------- */
+/*                                   Runtime                                  */
+/* -------------------------------------------------------------------------- */
+const bfFiles = validFiles.default;
+const query = `Which .bf files do you want to compile?\n${bfFiles
+    .map((file) => ` (${bfFiles.indexOf(file)}) ${file}\n`)
+    .join('')}\n> `;
+let pickedFiles = (0, readline_sync_1.question)(query).split(' ');
+pickedFiles = pickedFiles
+    .filter(function (index) {
+    return bfFiles.map((file) => `${bfFiles.indexOf(file)}`).includes(index);
+})
+    .map((answer) => bfFiles[parseInt(answer)].slice(0, -3));
+pickedFiles.forEach((file) => {
+    (0, compile_1.compile)(file);
+});
